b3c9a6474ea8412401ddf06c0621bb75a2233256
Monika <moncobra@comcast.net>
2023-01-27 10:11:04 -0800

    created GameContext component and handle function for box click

---
+++ src/components/context/GameContext.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const { useContext } = require('react');
+const { createContext } = require('react');
+
+const GameContext = createContext();
+
+const gameProvider = ({ children }) => {
+  const [currentPlayer, setCurrentPlayer] = useState('x');
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+  const [active, setActive] = useState(true);
+  const [gameMessage, setGameMessage] = useState('X&quot;s turn');
+
+  return (
+    <GameContext.Provider
+      value={{
+        currentPlayer,
+        setCurrentPlayer,
+        board,
+        setBoard,
+        active,
+        setActive,
+        gameMessage,
+        setGameMessage,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error('useGameContext must be used within GameProvider');
+  }
+  return context;
+};
+
+const handleBoxClick = () => {
+  console.log('box clicked');
+};
+
+export { gameProvider, useGameContext, handleBoxClick };
